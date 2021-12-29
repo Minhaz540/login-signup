4880fbb56788dbf0155a45b86f47d7ee83bc9d71
Minhaz <sharif35-3001@diu.edu.bd>
2021-12-29 16:12:19 +0600

    login process fixed

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,19 +1,27 @@
 const express = require("express");
+const app = express();
 const bcrypt = require("bcrypt");
-const multer = require("multer"); 
+const multer = require("multer");
 const userModel = require("../models/user.model");
 
 exports.login = (req, res) => {
-	res.render("login", { title: "Login" }); 
+	res.render("login", { title: "Login" });
 };
 
-exports.loginData = (async (req, res) => {
+exports.loginData = async (req, res) => {
 	const { email, password } = req.body;
-	const foundUserData = await userModel.find({ email: email, password: password });
-	if (!foundUserData && email !== foundUserData.email) {
-		res.send("Login failed! Invalid password or email");
-	} else {
-		res.render("Profile",{title: "Profile", email, password, imageName: foundUserData.imageName, username: foundUserData.username, role: foundUserData.role});
-	}
-});
+	const data = await userModel.find(req.body);
 
+	if (data[0].length != 0) {
+		res.render("Profile", {
+			title: "Profile",
+			email: data[0].email,
+			password: data[0].password,
+			imageName: data[0].imageName,
+			username: data[0].username,
+			role: data[0].role,
+		});
+	} else {
+		res.send(`Login failed! Invalid password or email.`);
+	}
+};
--- index.js
+++ index.js
@@ -8,9 +8,10 @@ const signupRoute = require("./routes/signup.route");
 const loginRoute = require("./routes/login.route");
 require("dotenv").config();
 
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
 const { PORT, HOSTNAME, DB_CONNECTION_STRING } = process.env;
 
 mongoose
